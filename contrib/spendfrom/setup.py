317df1381a5616a6c37bfe9aa31c12e207d6de72
Jimmy Pound <jimmy.pound@proton.me>
2024-07-28 12:25:01 +0200

    Remove existing content

--- contrib/spendfrom/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='btmspendfrom',
-      version='1.0',
-      description='Command-line utility for bitmark "coin control"',
-      author='Gavin Andresen',
-      author_email='gavin@bitmarkfoundation.org',
-      requires=['jsonrpc'],
-      scripts=['spendfrom.py'],
-      )
--- src/test/fork_tests/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-./init.sh
-./basic.sh
